79364441e3fb13722eeeca1a3a87900a7d481cd6
harshkuche23 <harshkuche23@gmail.com>
2026-01-19 19:54:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
     //Write your code here
+    const form = document.forms[0];
+
+    const firstName = form.fname.value.trim();
+    const lastName = form.lname.value.trim();
+
+    alert(firstName + " " + lastName);
+}
 
 }
